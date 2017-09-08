6a757e986409f56d10cc83804a525f14c7b672f2
Gustav Hedin <gustav.hedin.2@gmail.com>
2017-09-08 14:07:08 +0200

    Basisfunc och Basisfunc fixade

--- Blossoms/basisfunc.py
+++ Blossoms/basisfunc.py
@@ -4,37 +4,41 @@ Created on Thu Sep  7 09:28:57 2017
 @author: Hedin
 """
 
-from  scipy import *
-from  pylab import *
 import numpy as np
 
-
-# 
-def heaviside_a(the_x):
-    # step function 
-    if (the_x > 0):
-        the_result = 1
-    elif (the_x == 0):
-        the_result = 1
+# Define Heaviside Function:
+def heaviside_a(x):
+    if (x > 0):
+        result = 1
+    elif (x == 0):
+        result = 1
     else:
-        the_result = 0
-    return the_result
+        result = 0
+    return result
 
 
-
+# Define a function to calculate coefficient polynomials for the N's:
 def coef(u,u_knots,j,k):
     # u = np.linspace(u_knots[j-1],u_knots[j+4], num = 50)
     c = np.array([((u-u_knots[j-1])/(u_knots[j+k-1]-u_knots[j-1])),((u_knots[j+k]-u)/(u_knots[j+k]-u_knots[j]))])
     return c
 
 
-
-# Find N_j^k:
+# Define a function that computes basisfunction Nj(u), of degree k.
 def basisfunc(u,u_knots,j,k):
     if (k>0):
+        # If k>0, N is a function of N of lower degrees.  
         N = coef(u,u_knots,j,k)[0]*basisfunc(u,u_knots,j,k-1) + coef(u,u_knots,j,k)[1]*basisfunc(u,u_knots,j+1,k-1) 
     else:
+        # If k=0, N consists of Heavisides:
         N = heaviside_a(u-u_knots[j-1]) - heaviside_a(u - u_knots[j])
     return N;
-    
-    
+
+
+# Define a function that evaluates basisfunction over an interval u    
+def evalbasisfunc(u_knots,j,k):
+    u = np.linspace(u_knots[j-1],u_knots[j+3], num = 100)
+    N = np.zeros((1,len(u)))
+    for i in range(len(u)):
+        N[0,i] = basisfunc(u[i],u_knots,j,k)
+    return [u,N]
--- Blossoms/basisfunctest.py
+++ Blossoms/basisfunctest.py
@@ -3,35 +3,22 @@
 Created on Thu Sep  7 10:40:41 2017
 @author: Hedin
 """
-from  scipy import *
-from  pylab import *
+#from  scipy import *
+#from  pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
 import basisfunc as bf
 
+# Give desired values to u_knots and constants:
 u_knots = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.])
-
-
-
 j = 5
 k = 3
 
+# Evaluate the basis function:
+[u,N] = bf.evalbasisfunc(u_knots,j,k)
 
-
-# for i in range()
-u = np.linspace(u_knots[j-1],u_knots[j+3], num = 50)
-N = np.zeros((1,len(u)))
-for i in range(len(u)):
-    #print(u[i])
-    N[0,i] = bf.basisfunc(u[i],u_knots,j,k)
-
-#print(N)
-
-
+# Plot The basis function:
 plt.plot(u,N[0,:])
-plt.ylabel('Basis Function')
+plt.ylabel('Basis Function: N(u)')
+plt.xlabel('u')
 plt.show()
-
-
-
-# Hej hopp
