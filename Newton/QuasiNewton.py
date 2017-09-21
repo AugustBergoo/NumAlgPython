26b5a409477bc30d37ccf664c5bb3ad4d82a168e
Axiomel <31615210+Axiomel@users.noreply.github.com>
2017-09-21 17:57:04 +0200

    Quasi Newton
    First atempt at Quasi newton

--- Newton/QuasiNewton.py
+++ Newton/QuasiNewton.py
@@ -8,32 +8,53 @@ Created on Wed Sep 20 15:52:34 2017
 
 from scipy import *
 from pylab import *
+import numpy as np
 
 class QuasiNewton(GenericNewton):
+    # Assume we input objFunc and objGrad as pyton functions. E.g objFunc(xk) 
+    # will return the function value in xk. Remember lineaserch is a pyton
+    # function  always.
     
-    def __init__(self, objFunc, objGrad, linesearch, tol):
+    # We start by setting the Hessian to the unit matrix. This was legit
+    # according to the literature...
+    def __init__(self, objFunc, objGrad, linesearch, tol, initialGuess):
         super(tol)
-        pass
-    
-    def step(self, xk):
-        pass
+        self.objFunc = objFunc
+        self.objGrad = objGrad
+        self.linesearch = linesearch
+        self.H_k = np.eye(len(initalGuess))
         
+    
+    # Structure of the step method:
+        # A) set sk=-Hkkgk
+        # B) line Search along sk giving xk+1=xk+ alpha*sk
+        # C) update Hk giving, offcourse Hk+1
+    def step(self, xk):
+        #A)
+        g_k = self.objGrad(xk)
+        s_k = -self.H_k*g_k(xk)
+        #B)
+        alpha = linesearch(xk, s_k, self.objFunc, self.objGrad)
+        xnext = xk + aplha*s_k
+        #C)
+        self.H_k = self.updateB(xk,xnext)
+   
     # Check if possible to make an abstract updateB method.
         
 
 class GoodBroyden(QuasiNewton):
-    def updateB(self): 
+    def updateB(self,xk,xnext): 
         pass
 
 class BadBroyden(QuasiNewton):
-    def updateB(self): 
+    def updateB(self,xk,xnext): 
         pass
 
 class DFP(QuasiNewton):
-    def updateB(self): 
+    def updateB(self,xk,xnext): 
         pass
 
 class BFGS(QuasiNewton):
-    def updateB(self): 
+    def updateB(self,xk,xnext): 
         pass
         
