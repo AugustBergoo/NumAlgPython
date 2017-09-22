aa127c980d2c735184fc849b540955dcce6713e4
Axiomel <31615210+Axiomel@users.noreply.github.com>
2017-09-22 08:29:32 +0200

    QuasiNewton

--- Newton/QuasiNewton.py
+++ Newton/QuasiNewton.py
@@ -22,7 +22,7 @@ class QuasiNewton(GenericNewton):
         self.objFunc = objFunc
         self.objGrad = objGrad
         self.linesearch = linesearch
-        self.H_k = np.eye(len(initalGuess))
+        self.H_k = np.eye(len(initialGuess))
         
     
     # Structure of the step method:
@@ -32,7 +32,7 @@ class QuasiNewton(GenericNewton):
     def step(self, xk):
         #A)
         g_k = self.objGrad(xk)
-        s_k = -self.H_k*g_k(xk)
+        s_k = -self.H_k*g_k
         #B)
         alpha = linesearch(xk, s_k, self.objFunc, self.objGrad)
         xnext = xk + aplha*s_k
@@ -44,7 +44,7 @@ class QuasiNewton(GenericNewton):
 
 class GoodBroyden(QuasiNewton):
     def updateB(self,xk,xnext): 
-        pass
+        return
 
 class BadBroyden(QuasiNewton):
     def updateB(self,xk,xnext): 
@@ -57,4 +57,8 @@ class DFP(QuasiNewton):
 class BFGS(QuasiNewton):
     def updateB(self,xk,xnext): 
         pass
-        
+    
+    
+    
+a = GoodBroyden(objFunc,kdsjdkdds,ds sdds,,d,sd)
+a.findMin()
