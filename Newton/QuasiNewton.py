0f4b3c5e08feae48c36da89539d78cdd944b7a06
Axiomel <31615210+Axiomel@users.noreply.github.com>
2017-09-22 10:05:45 +0200

    QuasiNewton

--- Newton/QuasiNewton.py
+++ Newton/QuasiNewton.py
@@ -18,7 +18,7 @@ class QuasiNewton(GenericNewton):
     # We start by setting the Hessian to the unit matrix. This was legit
     # according to the literature...
     def __init__(self, objFunc, objGrad, linesearch, tol, initialGuess):
-        super(tol)
+        super(QuasiNewton, self).__init__(tol)
         self.objFunc = objFunc
         self.objGrad = objGrad
         self.linesearch = linesearch
@@ -32,7 +32,7 @@ class QuasiNewton(GenericNewton):
     def step(self, xk):
         #A)
         g_k = self.objGrad(xk)
-        s_k = -self.H_k*g_k
+        s_k = -self.H_k@g_k
         #B)
         alpha = linesearch(xk, s_k, self.objFunc, self.objGrad)
         xnext = xk + aplha*s_k
@@ -40,6 +40,8 @@ class QuasiNewton(GenericNewton):
         delta = xnext-xk
         gamma = self.objGrad(xnext)-self.objGrad(xk)
         self.H_k = self.updateB(delta,gamma)
+        
+        return xnext
    
     # Check if possible to make an abstract updateB method.
         
@@ -60,12 +62,9 @@ class DFP(QuasiNewton):
     def updateB(self,delta,gamma):
         deltaT = np.transpose(delta)
         gammaT = np.transpose(gamma)
-        term1 = self.H_k+(delta*deltaT/((deltaT)*gamma))
+        term1 = self.H_k+(delta*deltaT/((deltaT)gamma))
         term2 = (self.H_k*gamma*gammaT*self.H_k)/(gammaT*self.H_k*gamma)
-        return term1 - term2
-    
-    
-    
+        return term1 - term2      
     
 
 class BFGS(QuasiNewton):
@@ -73,7 +72,3 @@ class BFGS(QuasiNewton):
         return self.H_k + (1 + gamma.t*self.H_k*gamma/((delta.t)*gamma))*((delta*(delta.t))/((delta.t)*gamma)- (delta*(gamma.t)*self.H_k + self.H_k*gamma*(delta.t))/(delta*(delta.t))
         
     
-    
-    
-a = GoodBroyden(objFunc,kdsjdkdds,ds sdds,,d,sd)
-a.findMin()
