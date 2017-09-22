c46a401db69238b3a01c095328809bcb9c213a02
August Bergöö <august@bergoo.se>
2017-09-22 12:04:15 +0200

    Implemented linesearch

--- Newton/Linesearch.py
+++ Newton/Linesearch.py
@@ -8,13 +8,59 @@ Created on Wed Sep 20 15:25:00 2017
 
 from scipy import *
 from pylab import *
+import numpy as np
 
 class Linesearch():
     
     def inexactLinesearch(self, xk, dk, func, grad): # Notation: see Antoniou Lu pp.112
-        pass
+    """Armijo's rule"""
+        epsilon = 0.25
+        alpha = 2 
+        lamb = 2 # Initial guess for lambda.
+        
+        T = __make_T(func, grad, epsilon)
+        
+        while func(xk + lamb*dk) > T(lamb) and func(xk + alpha*lamb*dk) < T(alpha*lamb):
+            if func(xk + lamb*dk) > T(lamb): # The step was too large
+                lamb /= alpha
+            else: # The step was too small.
+                lamb *= alpha
+        
+        return lamb
+        
     
     def exactLinesearch(self, xk, dk, func, grad):
-        pass
+        """The bisection method"""
+        a = 0 # Lower interval.
+        b = 2 # Upper interval.
+        tol = 0.001
+
+        # Find the upper interval by using parts of Armijo's rule.
+        epsilon = 0.25
+        alpha = 2
+        T = __make_T(func, grad, epsilon)
+        while func(xk + alpha*b*dk) < T(alpha*b):
+            b *= alpha
+        
+        d_lamb = tol + 1
+        old_lamb = 0
+        while d_step > tol:
+            lamb = (a + b) / 2
+            if (grad(xk + lamb*dk) <= 0):
+                a = lamb
+            else:
+                b = lamb
+            
+            d_lamb = abs(old_lamb - lamb)
+            old_lamb = lamb
+        
+        return lamb
+        
+    
+    
+    def __make_T(func, grad, epsilon):
+        def T(step):
+            return func(xk) + epsilon*step*grad(xk)
+        return T
     
     
